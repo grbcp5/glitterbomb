741ed4b0613e4cb7963b2ec32b202e9404f9d570
grbcp5 <grbcp5@mst.edu>
2018-02-01 20:36:23 -0600

    Add more Puzzle functionality

--- Puzzle.cpp
+++ Puzzle.cpp
@@ -14,6 +14,7 @@
 #include "Puzzle.h"
 #include <fstream>
 
+
 /* Constructor with no grid initializer */
 Puzzle::Puzzle(
     const uint32 quota,
@@ -22,16 +23,21 @@ Puzzle::Puzzle(
     const uint32 grid_width,
     const uint32 grid_height,
     const uint32 pool_height,
-    const uint32 bonus_rules
+    const uint32 bonus_rules,
+    const uint32 total_height
 )
     :
+    m_grid( NULL ),
+    m_score( 0 ),
+    m_swaps_used( 0 ),
     m_quota( quota ),
     m_num_swaps( num_swaps ),
     m_num_device_types( num_device_types ),
     m_grid_width( grid_width ),
     m_grid_height( grid_height ),
     m_pool_height( pool_height ),
-    m_bonus_rules( bonus_rules )
+    m_bonus_rules( bonus_rules ),
+    m_total_height( total_height )
 {
 
   m_grid = new uint8 *[m_grid_height];
@@ -46,6 +52,7 @@ Puzzle::Puzzle(
   }
 }
 
+
 /* Deep copy grid */
 Puzzle::Puzzle(
     const uint32 quota,
@@ -55,51 +62,43 @@ Puzzle::Puzzle(
     const uint32 grid_height,
     const uint32 pool_height,
     const uint32 bonus_rules,
+    const uint32 total_height,
     const uint8 **grid
 )
     :
+    m_grid( NULL ),
+    m_score( 0 ),
+    m_swaps_used( 0 ),
     m_quota( quota ),
     m_num_swaps( num_swaps ),
     m_num_device_types( num_device_types ),
     m_grid_width( grid_width ),
     m_grid_height( grid_height ),
     m_pool_height( pool_height ),
-    m_bonus_rules( bonus_rules )
+    m_bonus_rules( bonus_rules ),
+    m_total_height( total_height )
 {
-
-  m_grid = new uint8 *[m_grid_height];
-  for ( int r = 0; r < m_grid_height; r++ ) {
-
-    m_grid[ r ] = new uint8[m_grid_width];
-    for ( int c = 0; c < m_grid_width; c++ ) {
-
-      m_grid[ r ][ c ] = grid[ r ][ c ];
-
-    }
-  }
+  m_grid = copyGrid( grid, m_grid_height, m_grid_width );
 }
+
 
 /* Copy Constructor */
 Puzzle::Puzzle( const Puzzle &cpy ) :
+    m_grid( NULL ),
+    m_score( cpy.m_score ),
+    m_swaps_used( cpy.m_swaps_used ),
     m_quota( cpy.m_quota ),
     m_num_swaps( cpy.m_num_swaps ),
     m_num_device_types( cpy.m_num_device_types ),
     m_grid_width( cpy.m_grid_width ),
     m_grid_height( cpy.m_grid_height ),
     m_pool_height( cpy.m_pool_height ),
-    m_bonus_rules( cpy.m_bonus_rules )
+    m_bonus_rules( cpy.m_bonus_rules ),
+    m_total_height( cpy.m_total_height )
 {
-  m_grid = new uint8 *[m_grid_height];
-  for ( int r = 0; r < m_grid_height; r++ ) {
-
-    m_grid[ r ] = new uint8[m_grid_width];
-    for ( int c = 0; c < m_grid_width; c++ ) {
-
-      m_grid[ r ][ c ] = cpy.m_grid[ r ][ c ];
-
-    }
-  }
+  m_grid = cpy.getCopyOfGrid();
 }
+
 
 /* Destructor */
 Puzzle::~Puzzle() {
@@ -109,6 +108,7 @@ Puzzle::~Puzzle() {
   delete [] m_grid;
 }
 
+/*   Commented out because not currently in use
 Puzzle *Puzzle::construct(std::istream &in) {
   uint32 quota, num_swaps, num_device_types, grid_width, grid_height,
       pool_height, bonus_rules;
@@ -125,7 +125,8 @@ Puzzle *Puzzle::construct(std::istream &in) {
       grid_width,
       grid_height,
       pool_height,
-      bonus_rules
+      bonus_rules,
+      pool_height + grid_height
   );
 
   for( int r = 0; r < result->m_grid_height; r++ ) {
@@ -138,6 +139,7 @@ Puzzle *Puzzle::construct(std::istream &in) {
   return result;
 }
 
+/*   Commented out because not currently in use
 Puzzle *Puzzle::construct(const char *fileName) {
   Puzzle *result = NULL;
 
@@ -147,7 +149,8 @@ Puzzle *Puzzle::construct(const char *fileName) {
   fin.close();
 
   return result;
-}
+} */
+
 
 std::ostream &operator<<(std::ostream &out, const Puzzle &p) {
 
@@ -166,4 +169,71 @@ std::ostream &operator<<(std::ostream &out, const Puzzle &p) {
   }
 
   return out;
+}
+
+void swap( uint8 **m_grid, Point from, Point to ) {
+
+  uint8 temp;
+
+  temp = m_grid[ to.row ][ to.col ];
+  m_grid[ to.row ][ to.col ] = m_grid[ from.row ][ from.col ];
+  m_grid[ from.row ][ from.col ] = temp;
+
+  return;
+}
+
+void Puzzle::swap( Point from, Point to ) {
+  ::swap( m_grid, from, to );
+}
+
+uint8 Puzzle::getDeviceType( Point p ) const {
+
+  /* Return no device if out of bounds */
+  if ( p.row >= m_grid_height || p.col >= m_grid_width ) {
+    return Puzzle::NO_DEVICE;
+  }
+
+  return m_grid[ p.row ][ p.col ];
+}
+
+
+uint8 Puzzle::getDeviceType( uint32 r, uint32 c ) const {
+  return getDeviceType( Point( r, c ));
+}
+
+
+uint8 **Puzzle::getCopyOfGrid() const {
+  return copyGrid(
+      ( const uint8 **const ) m_grid,
+      m_grid_height,
+      m_grid_width
+  );
+}
+
+uint8 **copyGrid(
+    const uint8 **const m_grid,
+    const uint32 m_grid_height,
+    const uint32 m_grid_width
+) {
+  /* Local Variables */
+  uint8 **result;
+
+  /* Create array of rows for grid of equal length */
+  result = new uint8 *[m_grid_height];
+
+  /* For each row index */
+  for ( int r = 0; r < m_grid_height; ++r ) {
+
+    /* Create new row of equal width */
+    result[ r ] = new uint8[m_grid_width];
+
+    /* For each column position in row */
+    for ( int c = 0; c < m_grid_width; ++c ) {
+
+      /* Copy value */
+      result[ r ][ c ] = m_grid[ r ][ c ];
+    }
+  }
+
+  return result;
 }
--- Puzzle.h
+++ Puzzle.h
@@ -14,6 +14,10 @@ private:
     uint8 **m_grid;
 
 public:
+    static const uint8 NO_DEVICE = 0;
+
+    uint32 m_score;
+    uint32 m_swaps_used;
     const uint32 m_quota;
     const uint32 m_num_swaps;
     const uint32 m_num_device_types;
@@ -21,6 +25,7 @@ public:
     const uint32 m_grid_height;
     const uint32 m_pool_height;
     const uint32 m_bonus_rules;
+    const uint32 m_total_height;
 
     /* No grid initializer */
     Puzzle(
@@ -30,7 +35,8 @@ public:
         const uint32 grid_width,
         const uint32 grid_height,
         const uint32 pool_height,
-        const uint32 bonus_rules
+        const uint32 bonus_rules,
+        const uint32 total_height
     );
 
     /* Deep grid copy */
@@ -42,6 +48,7 @@ public:
         const uint32 grid_height,
         const uint32 pool_height,
         const uint32 bonus_rules,
+        const uint32 total_height,
         const uint8 **grid
     );
 
@@ -51,13 +58,31 @@ public:
     /* Safe destructor */
     ~Puzzle();
 
+    /* Member functions */
+    void swap( Point from, Point to );
+
+    uint8 getDeviceType( Point p ) const;
+
+    uint8 getDeviceType( uint32 r, uint32 c ) const;
+
+    uint8 **getCopyOfGrid() const;
+
     /* Static functions */
+/*   Commented out because not currently in use
     static Puzzle *construct(std::istream &in);
-    static Puzzle *construct(const char *fileName);
+/*   Commented out because not currently in use
+    static Puzzle *construct(const char *fileName); */
 
     /* Friend functions */
     friend std::ostream &operator<<(std::ostream &out, const Puzzle &p);
 };
 
+void swap( uint8 **grid, Point from, Point to );
+
+uint8 **copyGrid(
+    const uint8 **const m_grid,
+    const uint32 m_grid_height,
+    const uint32 m_grid_width
+);
 
 #endif //GLITTERBOMB_PUZZLE_H
