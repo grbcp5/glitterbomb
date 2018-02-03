759dd425dc390ab2e627a4d36809c1163dfdaaf6
grbcp5 <grbcp5@mst.edu>
2018-02-03 17:26:16 -0600

    Add Search Node

--- Searcher.cpp
+++ Searcher.cpp
@@ -15,9 +15,11 @@
 
 /* Default Constructor */
 PuzzleSolution::PuzzleSolution()
-    : initialState( NULL ),
+    : solutionExists( false ),
+      initialState( NULL ),
       numMovesToSolution( 0 ),
       moves( NULL ) {}
+
 
 /* De-constructor */
 PuzzleSolution::~PuzzleSolution() {
@@ -25,4 +27,115 @@ PuzzleSolution::~PuzzleSolution() {
   delete initialState;
   delete moves;
 
+}
+
+/* Search Node ****************************************************************/
+
+SearchNode::SearchNode()
+    : m_initialState( NULL ),
+      m_parent( NULL ),
+      m_action( NULL ) {}
+
+
+SearchNode::SearchNode( const Puzzle *initialState )
+    : m_initialState( NULL ),
+      m_parent( NULL ),
+      m_action( NULL ) {
+
+  m_initialState = new Puzzle( *( initialState ));
+}
+
+
+SearchNode::SearchNode( const SearchNode *parent, const Move *action )
+    : m_initialState( NULL ) {
+
+  m_parent = new SearchNode( *( parent ));
+  m_action = new Move( *( action ));
+
+}
+
+
+SearchNode::SearchNode(
+    const Puzzle *initialState,
+    const SearchNode *parent,
+    const Move *action ) {
+
+  m_initialState = new Puzzle( *( initialState ));
+  m_parent = new SearchNode( *( parent ));
+  m_action = new Move( *( action ));
+
+}
+
+
+SearchNode::SearchNode( const SearchNode &cpy ) {
+
+  m_initialState = new Puzzle( *( cpy.m_initialState ));
+  m_parent = new SearchNode( *( cpy.m_parent ));
+  m_action = new Move( *( cpy.m_action ));
+
+}
+
+
+SearchNode SearchNode::operator=( const SearchNode &rhs ) {
+
+  if ( &rhs == this ) {
+    return *this;
+  }
+
+  delete m_parent;
+  m_parent = new SearchNode( *( rhs.m_parent ));
+
+  delete m_action;
+  m_action = new Move( *( rhs.m_action ));
+
+  return *this;
+}
+
+
+SearchNode::~SearchNode() {
+
+  delete m_parent;
+  delete m_action;
+
+}
+
+
+Puzzle *SearchNode::getState() const {
+
+  /* Local variables */
+  Puzzle *state = NULL;
+
+  /* Recursive base case */
+  if ( m_parent == NULL ) {
+    return new Puzzle( *( m_initialState ));
+  }
+
+  /* Get result after all parent manipulations */
+  state = m_parent->getState();
+
+  /* Make manipulation */
+  if ( m_action != NULL ) {
+    state->makeMove( *m_action );
+  }
+
+  return state;
+}
+
+
+SearchNode *SearchNode::getParent() const {
+  return new SearchNode( *( m_parent ));
+}
+
+
+Move *SearchNode::getAction() const {
+  return new Move( *( m_action ));
+}
+
+
+uint32 SearchNode::getPathCost() const {
+  if ( m_parent == NULL ) {
+    return 0;
+  }
+
+  return m_parent->getPathCost() + EPSILLON;
 }
--- Searcher.h
+++ Searcher.h
@@ -5,13 +5,14 @@
 #ifndef GLITTERBOMB_SEARCHER_H
 #define GLITTERBOMB_SEARCHER_H
 
-#include <vector>
-
 #include "types.h"
 #include "Puzzle.h"
 
+const uint32 EPSILLON = 1u;
+
 struct PuzzleSolution {
 
+    bool solutionExists;
     const Puzzle *initialState;
     uint32 numMovesToSolution;
     const Move *moves;
@@ -21,6 +22,42 @@ struct PuzzleSolution {
 
     /* De-constructor */
     ~PuzzleSolution();
+};
+
+class SearchNode {
+
+private:
+    const Puzzle *m_initialState;
+    const SearchNode *m_parent;
+    const Move *m_action;
+
+public:
+
+    SearchNode();
+
+    SearchNode( const Puzzle *initialState );
+
+    SearchNode( const SearchNode *parent, const Move *action );
+
+    SearchNode(
+        const Puzzle *initialState,
+        const SearchNode *parent,
+        const Move *action
+    );
+
+    SearchNode( const SearchNode &cpy );
+
+    SearchNode operator=( const SearchNode &rhs );
+
+    ~SearchNode();
+
+    Puzzle *getState() const;
+
+    SearchNode *getParent() const;
+
+    Move *getAction() const;
+
+    uint32 getPathCost() const;
 };
 
 class Searcher {
